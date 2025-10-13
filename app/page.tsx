242787f163c931dd139c678d1cacd2719eec77c4
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-13 12:55:58 -0300

    projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
+import { Projects } from "@/components/projects";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Projects />
     </>
   );
 }
