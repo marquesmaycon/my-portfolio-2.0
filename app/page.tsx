4679841188ef0fa8dccdbbf2e29fe91c2c99b3ab
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-14 16:00:09 -0300

    testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 import { Projects } from "@/components/projects";
 import { Tape } from "@/components/tape";
+import { Testimonials } from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Tape />
+      <Testimonials />
     </>
   );
 }
