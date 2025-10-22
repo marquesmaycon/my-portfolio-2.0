23532ce654b578b4962048315481d90adf2122d9
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-21 23:11:50 -0300

    projects scroll effect

--- components/projects.tsx
+++ components/projects.tsx
@@ -58,54 +58,57 @@ export function Projects() {
           description="Veja como transformei ideias em realidade com meus projetos recentes"
         />
         <div className="flex flex-col gap-20 mt-10 md:mt-20">
-          {portfolioProjects.map((project, index) => (
-            <Card
-              key={project.title}
-              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
-            >
-              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                <div className="lg:pb-16">
-                  <div className="gradient-primary inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                    <span>{project.company}</span>
-                    <span>&bull;</span>
-                    <span>{project.year}</span>
-                  </div>
+          {portfolioProjects.map(
+            ({ title, company, year, results, link, image }, index) => (
+              <Card
+                key={title}
+                className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+                style={{ top: `${64 + index * 40}px` }}
+              >
+                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                  <div className="lg:pb-16">
+                    <div className="gradient-primary inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                      <span>{company}</span>
+                      <span>&bull;</span>
+                      <span>{year}</span>
+                    </div>
 
-                  <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">
-                    {project.title}
-                  </h3>
-                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                    {project.results.map((result) => (
-                      <li
-                        key={result.title}
-                        className="flex gap-2 text-sm md:text-base text-white/50"
+                    <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">
+                      {title}
+                    </h3>
+                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                    <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                      {results.map((result) => (
+                        <li
+                          key={result.title}
+                          className="flex gap-2 text-sm md:text-base text-white/50"
+                        >
+                          <CheckCircle className="size-5 md:size-6" />
+                          <span>{result.title}</span>
+                        </li>
+                      ))}
+                    </ul>
+                    <a href={link} target="_blank">
+                      <button
+                        type="button"
+                        className="bg-white text-gray-950 h-12 w-full px-6 md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 cursor-pointer hover:bg-white/70 transition-colors duration-300"
                       >
-                        <CheckCircle className="size-5 md:size-6" />
-                        <span>{result.title}</span>
-                      </li>
-                    ))}
-                  </ul>
-                  <a href={project.link} target="_blank">
-                    <button
-                      type="button"
-                      className="bg-white text-gray-950 h-12 w-full px-6 md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 cursor-pointer hover:bg-white/70 transition-colors duration-300"
-                    >
-                      <span>View Project</span>
-                      <ArrowUpRight className="size-4" />
-                    </button>
-                  </a>
+                        <span>View Project</span>
+                        <ArrowUpRight className="size-4" />
+                      </button>
+                    </a>
+                  </div>
+                  <div className="relative">
+                    <Image
+                      src={image}
+                      alt={title}
+                      className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                    />
+                  </div>
                 </div>
-                <div className="relative">
-                  <Image
-                    src={project.image}
-                    alt={project.title}
-                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
-                  />
-                </div>
-              </div>
-            </Card>
-          ))}
+              </Card>
+            ),
+          )}
         </div>
       </div>
     </section>
