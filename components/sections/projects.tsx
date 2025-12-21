73a81be66c494aaead0e510153ef440b95f2443b
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-21 15:59:10 -0300

    ask room project added

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import askRoomPrintSCreen from "@/assets/images/ask-room.png";
 import dummyUsersPrintScreen from "@/assets/images/dummy-users.png";
 import habitTrackerPrintScreen from "@/assets/images/habit-tracker.png";
+import marketplacePrintSCreen from "@/assets/images/marketplace-nextjs.png";
 import nextSaasRbacPrintScreen from "@/assets/images/next-saas-rbac.png";
-import marketplacePrintSCreen from "@/assets/images/marketplace-nextjs.png"
 
 import { Card } from "../card";
 import { SectionHeader } from "../section-header";
@@ -12,11 +12,32 @@ import { CheckCircle } from "../svgs/check-circle";
 
 const portfolioProjects = [
   {
+    tag: "Inteligência Artificial",
+    year: "2025",
+    title: "Ask Room + Gemini AI",
+    topics: [
+      {
+        title:
+          "Gemini AI, Next.js, TanStack Query & Form, Prisma ORM, Hono.js, Shadcn UI, Tailwind CSS, TypeScript",
+      },
+      {
+        title:
+          "Projeto full-stack que permite a criação de salas de conhecimento, onde usuários podem adicionar conteúdos em texto ou áudio e, a partir desse contexto, outras pessoas fazem perguntas que são respondidas por uma IA exclusivamente com base no material fornecido.",
+      },
+    ],
+    repository: "https://github.com/marquesmaycon/ask-room",
+    image: askRoomPrintSCreen,
+    wip: true,
+  },
+  {
     tag: "Fluxo de Checkout",
     year: "2025",
     title: "Marketplace Next.js",
     topics: [
-      { title: "React, Next, TanStack Query & Form, Shadcn UI, Tailwind CSS, TypeScript." },
+      {
+        title:
+          "React, Next.js, TanStack Query & Form, Shadcn UI, Tailwind CSS, TypeScript.",
+      },
       {
         title:
           "Aplicação moderna desenvolvida para demonstrar um fluxo completo de checkout com autenticação, carrinho e múltiplos métodos de pagamento.",
@@ -27,19 +48,21 @@ const portfolioProjects = [
     image: marketplacePrintSCreen,
   },
   {
-    tag: "API externas",
+    tag: "Mono Repo",
     year: "2025",
-    title: "Dummy Users",
+    title: "Next.js Saas RBAC",
     topics: [
-      { title: "React, TanStack Query, Ant Design, TypeScript." },
       {
         title:
-          "Sistema de gerenciamento de usuários que demonstra implementação de autenticação, CRUD de usuários e gerenciamento de estado e cache com TanStack Query.",
+          "Next.js, Turborepo, PostgreSQL, Prisma ORM, Fastify, Node, Shadcn/ui, Tailwind CSS, TypeScript.",
+      },
+      {
+        title:
+          "SaaS multi-tenant com autenticação robusta e sistema de autorização baseado em RBAC (Role-Based Access Control). Desenvolvido com as melhores práticas e arquitetura monorepo para máxima escalabilidade e reutilização de código.",
       },
     ],
-    demo: "https://dummy-users.mklly.com.br/",
-    repository: "https://github.com/marquesmaycon/dummy-users",
-    image: dummyUsersPrintScreen,
+    repository: "https://github.com/marquesmaycon/next-saas-rbac",
+    image: nextSaasRbacPrintScreen,
   },
   {
     tag: "Full Stack",
@@ -60,21 +83,19 @@ const portfolioProjects = [
     image: habitTrackerPrintScreen,
   },
   {
-    tag: "Mono Repo",
+    tag: "API externas",
     year: "2025",
-    title: "Next Saas RBAC",
+    title: "Dummy Users",
     topics: [
+      { title: "React, TanStack Query, Ant Design, TypeScript." },
       {
         title:
-          "Next.js, Turborepo, PostgreSQL, Prisma ORM, Fastify, Node, Shadcn/ui, Tailwind CSS, TypeScript.",
-      },
-      {
-        title:
-          "SaaS multi-tenant com autenticação robusta e sistema de autorização baseado em RBAC (Role-Based Access Control). Desenvolvido com as melhores práticas e arquitetura monorepo para máxima escalabilidade e reutilização de código.",
+          "Sistema de gerenciamento de usuários que demonstra implementação de autenticação, CRUD de usuários e gerenciamento de estado e cache com TanStack Query.",
       },
     ],
-    repository: "https://github.com/marquesmaycon/next-saas-rbac",
-    image: nextSaasRbacPrintScreen,
+    demo: "https://dummy-users.mklly.com.br/",
+    repository: "https://github.com/marquesmaycon/dummy-users",
+    image: dummyUsersPrintScreen,
   },
 ];
 
@@ -89,19 +110,28 @@ export function Projects() {
         />
         <div className="flex flex-col gap-20 mt-10 md:mt-20">
           {portfolioProjects.map(
-            ({ title, tag, year, topics, demo, repository, image }, index) => (
+            (
+              { title, tag, year, topics, demo, repository, image, wip },
+              index,
+            ) => (
               <Card
                 key={title}
                 className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
                 style={{ top: `${64 + index * 40}px` }}
               >
                 <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                  <div className="lg:pb-16">
+                  <div className="lg:pb-16 relative">
                     <div className="gradient-primary inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                       <span>{tag}</span>
                       <span>&bull;</span>
                       <span>{year}</span>
                     </div>
+
+                    {wip && (
+                      <span className="p-1 absolute top-0 right-0 bg-card border-white/33 border-1 rounded-full text-xs">
+                        🔨 Em construção
+                      </span>
+                    )}
 
                     <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">
                       {title}
@@ -114,7 +144,9 @@ export function Projects() {
                           className="flex gap-2 text-sm md:text-base text-white/50"
                         >
                           <CheckCircle className="size-5 md:size-6 shrink-0" />
-                          <span className="line-clamp-3 md:line-clamp-none">{result.title}</span>
+                          <span className="line-clamp-3 md:line-clamp-none">
+                            {result.title}
+                          </span>
                         </li>
                       ))}
                     </ul>
@@ -138,6 +170,16 @@ export function Projects() {
                           <ArrowUpRight className="size-4" />
                         </button>
                       </a>
+                    )}
+                    {wip && (
+                      <button
+                        type="button"
+                        className="bg-white text-gray-950 h-11 md:h-12 w-full px-6 md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-4 md:mt-8 cursor-not-allowed opacity-50"
+                        disabled
+                      >
+                        <span>Demo em breve</span>
+                        <ArrowUpRight className="size-4" />
+                      </button>
                     )}
                   </div>
                   <div className="relative">
